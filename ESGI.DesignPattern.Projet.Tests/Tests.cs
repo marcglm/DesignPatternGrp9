1a5dba3541c87b42b8983aa0d20ae4c8d9d4d125
marcglm <marcghalem@hotmail.fr>
2020-06-30 14:30:40 +0200

    second commit

---
+++ ESGI.DesignPattern.Projet.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace ESGI.DesignPattern.Projet.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Checkout()
+        {
+            
+        }
+
+    }
+}
+
---
+++ ESGI.DesignPattern.Projet/UserConfirmation.cs
@@ -0,0 +1,23 @@
+﻿using PresentationFake;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Windows;
+
+namespace ESGI.DesignPattern.Projet
+{
+    public class UserConfirmation
+    {
+        public bool Accepted { get; private set; }
+
+        public UserConfirmation(string message)
+        {
+            var result = MessageBox.Show(
+                 message,
+                 "Choose Option",
+                 MessageBoxButtons.YES);
+
+            this.Accepted = result == MessageBoxResult.YES;
+        }
+    }
+}
